539748263f53be2e3a2c2518623e13cfe5af71e4
leovt <leonhard.vogt@gmx.ch>
2025-11-07 22:10:51 +0100

    setup project structure with cabal

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
--- File: Main.hs
+module Main where
+
 import System.Environment (getArgs, getProgName)
 import System.Exit (die)
 import System.IO (appendFile)
 import System.Process (readProcess)
 import Lexer (lexer)
+import Parser (parser)
 
 main :: IO ()
 main = do
@@ -15,6 +17,7 @@ main = do
     case args of
         [input, "--lex"] -> clex input
         ["--lex", input] -> clex input
+        ["--parse", input] -> cparse input
 
 
 clex :: String -> IO ()
@@ -26,4 +29,12 @@ clex filename = do
 
 
 
+cparse :: String -> IO ()
+cparse filename = do
+    content <- readProcess "gcc" ["-E", "-P", filename] ""
+    case lexer content of 
+        Left errormsg -> die errormsg
+        Right tokens -> case parser tokens of
+            Left errormsg -> die errormsg
+            Right _ -> print "Success!"
 
